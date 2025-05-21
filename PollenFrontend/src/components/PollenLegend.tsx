427270138fd067837094686858ecd8b1695cf94b
Max Veldhuis <max.veldhuis@student.hu.nl>
2025-05-21 13:59:41 +0200

    Dark mode

--- PollenFrontend/src/components/PollenLegend.tsx
+++ PollenFrontend/src/components/PollenLegend.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { pollenMeta, PollenTypes } from './PollenMap';
 
 type PollenLegendProps = {
@@ -13,38 +12,42 @@ export const PollenLegend = ({ pollenType }: PollenLegendProps) => {
         rgba(${baseColor.join(',')}, 1))`;
 
     return (
-        <div style={{
-            position: 'absolute',
-            bottom: '20px',
-            left: '20px',
-            background: 'white',
-            padding: '10px',
-            borderRadius: '8px',
-            boxShadow: '0 0 6px rgba(0,0,0,0.3)',
-            zIndex: 1000,
-            fontSize: '0.9rem',
-            maxWidth: '300px',
-        }}>
+        <div
+            className="pollenlegend"
+            style={{
+                position: 'absolute',
+                bottom: '20px',
+                left: '20px',
+                background: 'white',
+                padding: '10px',
+                borderRadius: '8px',
+                boxShadow: '0 0 6px rgba(0,0,0,0.3)',
+                zIndex: 1000,
+                fontSize: '0.9rem',
+                maxWidth: '300px',
+            }}
+        >
             <div style={{ marginTop: '6px' }}>
-                <div style={{
-                    background: gradient,
-                    height: '20px',
-                    borderRadius: '4px',
-                    marginBottom: '6px'
-                }} />
-                <div style={{
-                    display: 'flex',
-                    gap: '75px',
-                    justifyContent: 'space-between',
-                    fontSize: '0.8rem'
-                }}>
+                <div
+                    style={{
+                        background: gradient,
+                        height: '20px',
+                        borderRadius: '4px',
+                        marginBottom: '6px',
+                    }}
+                />
+                <div
+                    style={{
+                        display: 'flex',
+                        gap: '75px',
+                        justifyContent: 'space-between',
+                        fontSize: '0.8rem',
+                    }}
+                >
                     <span>Weinig ({min})</span>
                     <span>Veel ({max})</span>
                 </div>
             </div>
-            {/* <div style={{ marginTop: '6px', fontSize: '0.8rem', color: '#555' }}>
-                Kleurintensiteit geeft hoeveelheid {pollenMeta[pollenType].rawName}-pollen aan.
-            </div> */}
         </div>
     );
 };
