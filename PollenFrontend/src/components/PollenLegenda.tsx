3cd15dd5200d12c74ac8da4f83ab57683a1f123a
Bas Verdoorn <basverdoorn@hotmail.com>
2025-06-03 20:02:14 +0200

    styling verbeterd zodat: - gradient kleuren overeenkomen met kaart - border voor duidelijker contrast - legenda blijft zelfde grootte ook tussen verschillende lengte max.

--- PollenFrontend/src/components/PollenLegenda.tsx
+++ PollenFrontend/src/components/PollenLegenda.tsx
@@ -8,45 +8,19 @@ export const PollenLegenda = ({ pollenType }: PollenLegendaProps) => {
     const { baseColor, min, max } = pollenMeta[pollenType];
 
     const gradient = `linear-gradient(to right,
-        rgba(${baseColor.join(',')}, 0.1),
+        rgba(240, 240, 240),
         rgba(${baseColor.join(',')}, 1))`;
 
     return (
-        <div
-            className="pollenLegenda"
-            style={{
-                position: 'absolute',
-                bottom: '20px',
-                left: '20px',
-                background: 'white',
-                padding: '10px',
-                borderRadius: '8px',
-                boxShadow: '0 0 6px rgba(0,0,0,0.3)',
-                zIndex: 1000,
-                fontSize: '0.9rem',
-                maxWidth: '300px',
-            }}
-        >
-            <div style={{ marginTop: '6px' }}>
-                <div
-                    style={{
-                        background: gradient,
-                        height: '20px',
-                        borderRadius: '4px',
-                        marginBottom: '6px',
-                    }}
-                />
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '75px',
-                        justifyContent: 'space-between',
-                        fontSize: '0.8rem',
-                    }}
-                >
-                    <span>Weinig ({min})</span>
-                    <span>Veel ({max})</span>
-                </div>
+        <div className="pollenLegenda">
+            <div className="pollenLegenda-gradient"
+                style={{
+                    background: gradient,
+                }}
+            />
+            <div className="pollenLegenda-text">
+                <span>Weinig ({min})</span>
+                <span>Veel ({max})</span>
             </div>
         </div>
     );
